d37c890de37005a90da952d58ab40de832af6b47
Samibgh <78345903+Samibgh@users.noreply.github.com>
2023-01-07 17:14:36 +0100

    Update fouilleDonnee.py

--- fouilleDonnee.py
+++ fouilleDonnee.py
@@ -42,4 +42,74 @@ data.FlagImpaye.value_counts()
 data[['Date','Heure_split']] = data.DateTransaction.str.split(expand=True)
 
 
+
+index = 0
+for i in data['Date'] : 
+    
+    
+    if i =="2017-09-01":
+        break
+    else:
+        index += 1
+        train = data.iloc[:index, :]
+
+
+test = data.iloc[index:, :]
+
+
+
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+for i in range(train.shape[1]):
+    train.loc[:,i] = le.fit_transform(train.loc[:,i])
+    
+    test.loc[:,i] = le.fit_transform(test.loc[:,i])
+
+
+Xtrain = train.drop(["FlagImpaye"], axis = 1)
+Ytrain = train.FlagImpaye
+Xtest  = test.drop(["FlagImpaye"], axis = 1)
+Ytest  = test.FlagImpaye
+
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import f1_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import GradientBoostingClassifier
+
+names=[]
+f1score_ =[]
+
+models={'SVC': SVC(),
+       'RandomForest': RandomForestClassifier(),
+       'DecisionTree': DecisionTreeClassifier(),
+       'Naïve Bayes': GaussianNB(), 
+       'Neural Network': MLPClassifier(),
+       'knn' : KNeighborsClassifier(),
+       'LDA': LinearDiscriminantAnalysis(),
+       'GradientBoosting' : GradientBoostingClassifier(), 
+       }
+
+#X = parcDisney.drop(["Note"], axis = 1)
+
+for name, model in models.items():
+    name_model = model
+    name_fit = name_model.fit(Xtrain,Ytrain)
+    name_pred = name_fit.predict(Xtest)
+    f1score = f1_score(Ytest,name_pred, average = "macro")
+    names.append(name)
+    f1score_.append(f1score)
+
+score_df = pd.DataFrame(zip(names, f1score_))
+score_df.columns = ["Nom", "Score"]
+
+
+
     
