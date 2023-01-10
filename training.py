df1a32260a9859d83c49c29dca07a34303b5740d
Samibgh <78345903+Samibgh@users.noreply.github.com>
2023-01-10 20:55:33 +0100

    one class svm

--- training.py
+++ training.py
@@ -14,6 +14,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from imblearn.over_sampling import BorderlineSMOTE
 import dask.dataframe as dd
 import fastparquet
+from sklearn.svm import OneClassSVM
 try:
     os.chdir("C:/Users/Sam/Documents/SISE/Fouille de données")
 except:
@@ -21,6 +22,9 @@ except:
     
 train = pd.read_parquet('train.parquet.gzip')
 test = pd.read_parquet('test.parquet.gzip')
+train_test = train.sample(n = 100000)
+test_test = test.sample(n = 100000)
+
 
 train.info()
 test.info()
@@ -51,7 +55,7 @@ import joblib
 names=[]
 f1score_ =[]
 
-models={'SVC': SVC(),
+models={'SVC': SVC(kernel='rbf'),
        'RandomForest': RandomForestClassifier(),
        'DecisionTree': DecisionTreeClassifier(),
        'Naïve Bayes': GaussianNB(), 
@@ -60,13 +64,21 @@ models={'SVC': SVC(),
        'LDA': LinearDiscriminantAnalysis(),
        'GradientBoosting' : GradientBoostingClassifier(), 
        }
+
+
+models2={'SVC': OneClassSVM(),
+       'RandomForest': RandomForestClassifier(),
+       'DecisionTree': DecisionTreeClassifier(),
+       'knn' : KNeighborsClassifier(),
+       'GradientBoosting' : GradientBoostingClassifier(), 
+       }
    
 def test():
     
      sm = BorderlineSMOTE(random_state = 0)
      XBdSmote , YBdSmote = sm.fit_resample(Xtrain, Ytrain)
         
-     for name, model in models.items(): 
+     for name, model in models2.items(): 
           
         name_model = model
         name_fit = name_model.fit(XBdSmote , YBdSmote)
@@ -84,3 +96,33 @@ def test():
 results = Parallel(n_jobs=3)(delayed(test)() for _ in range(1))
 
 results.to_csv("res.csv")
+
+
+
+###############################"one class svm#####################
+clf = OneClassSVM(gamma='auto').fit(Xtrain)
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+
+prediction = clf.predict(Xtest)
+prediction = [1 if i==-1 else 0 for i in prediction]
+print(classification_report(Xtest, prediction))
+
+
+score = clf.score_samples(Xtest)
+score_threshold = np.percentile(score, 2)
+customized_prediction = [1 if i < score_threshold else 0 for i in score]
+print(classification_report(Ytest, customized_prediction))
+
+
+df_test = pd.DataFrame(Xtest, columns=['feature1', 'feature2'])
+df_test['y_test'] = Ytest
+df_test['one_class_svm_prediction'] = prediction
+df_test['one_class_svm_prediction_cutomized'] = customized_prediction
+fig, (ax0, ax1, ax2)=plt.subplots(1,3, sharey=True, figsize=(20,6))
+ax0.set_title('Original')
+ax0.scatter(df_test['feature1'], df_test['feature2'], c=df_test['y_test'], cmap='rainbow')
+ax1.set_title('One-Class SVM Predictions')
+ax1.scatter(df_test['feature1'], df_test['feature2'], c=df_test['one_class_svm_prediction'], cmap='rainbow')
+ax2.set_title('One-Class SVM Predictions With Customized Threshold')
+ax2.scatter(df_test['feature1'], df_test['feature2'], c=df_test['one_class_svm_prediction_cutomized'], cmap='rainbow')
